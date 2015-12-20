2109f7ce3d5b5a7e5ee864932dfd101429711711
prasannabe2004 <prasannabe2004@gmail.com>
2015-12-20 12:19:04 -0600

    Explaing comma operator associativity is left to right

--- expertc/comma.c
+++ expertc/comma.c
@@ -6,19 +6,44 @@ void foo(void)
 }
 void bar(void)
 {
- printf("Quiz");
+ printf("Quiz\n");
 }
- 
+
+int f()
+{
+	printf("f\n");
+	return 1;
+}
+
+int g()
+{
+	printf("g\n");
+	return 2;
+} 
+
+int h()
+{
+	printf("h\n");
+	return 3;
+}  
 int main()
 {
-	// Left to Right
+	// Left to Right first foo and then bar
 	(foo(), bar());
  
 	int x;
 
-	// Left to Right
+	// Left to Right gives 3
 	x = (2,3);
- 
+
+	// f, g, h
+	x = f() + g() * h();
 	printf("%d\n", x);
+	
+	// a = 2
+	int a, b = 1, c = 2;
+	a = b = c;
+	printf("%d\n", a);
+	
 	return 0;
 }
