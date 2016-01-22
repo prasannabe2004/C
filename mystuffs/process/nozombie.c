7c35553951b14702694a064cc51e02273781a199
prasannabe2004 <prasannabe2004@gmail.com>
2016-01-21 20:54:40 -0600

    Process state programs and cleanup of system programs.

---
+++ mystuffs/process/nozombie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	int ret = fork();
+
+	switch(ret)
+	{
+		case -1:
+			exit(EXIT_FAILURE);
+		case 0:
+			printf("Child process\n");
+			break;
+		default:
+			printf("parent process\n");
+			wait();
+			pause();
+	}
+	exit(EXIT_SUCCESS);
+}
