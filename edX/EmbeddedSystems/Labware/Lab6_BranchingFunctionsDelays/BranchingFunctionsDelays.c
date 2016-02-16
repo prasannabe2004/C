0ec1fc80d22592955a39115ae43bfe2c3dbb747b
prasannabe2004 <prasannabe2004@gmail.com>
2016-02-15 22:08:23 -0600

    Lab6 simulation completed

--- edX/EmbeddedSystems/Labware/Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
+++ edX/EmbeddedSystems/Labware/Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
@@ -20,6 +20,8 @@
 #define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
 #define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
 #define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
 #define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
 #define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
@@ -29,12 +31,53 @@
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
-int main(void){ unsigned long volatile delay;
+void PortF_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay   
+  //GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  //GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0     
+}
+
+void Delay100ms(unsigned long time){
+  unsigned long i;
+  while(time > 0){
+    i = 1333333;  // this number means 100ms
+    while(i > 0){
+      i = i - 1;
+    }
+    time = time - 1; // decrements every 100 ms
+  }
+}
+
+int main(void){ 
+	unsigned long volatile delay;
+	unsigned long In;
   TExaS_Init(SW_PIN_PF4, LED_PIN_PF2);  // activate grader and set system clock to 80 MHz
   // initialization goes here
-
+	PortF_Init();
   EnableInterrupts();           // enable interrupts for the grader
+	
+	GPIO_PORTF_DATA_R = 0x04;
+	
   while(1){
-    // body goes here
+    // body goes here	
+		Delay100ms(1);
+		In = GPIO_PORTF_DATA_R&0x10;
+		if(In == 0x00)
+		{
+			//Switch is pressed toggel PF2
+			GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ (1 << 2);
+		}
+		else
+		{
+			//Switch is not pressed, set PF2 to on
+			GPIO_PORTF_DATA_R = 0x04;
+		}
   }
 }
